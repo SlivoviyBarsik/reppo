d6167b8c802d4dab4fd06cd13c731fd9ea054d7e
Anastasiia Pedan <pedanana@Anastasiias-MacBook-Pro.local>
2026-01-08 08:39:06 -0700

    added scripts for new perturbed lang runs

---
+++ scripts/paper_experiments/run_pert_ent_var.sh
@@ -0,0 +1,24 @@
+# no pert (to double check)
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.0 0.0 0.125
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.0 0.0 0.25
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.0 0.0 1.0
+
+
+# body perturbations
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.125 0.0 0.125
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.125 0.0 0.25
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.125 0.0 1.0
+
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.25 0.0 0.125
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.25 0.0 0.25
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.25 0.0 1.0
+
+
+# gravity perturbations
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.0 0.25 0.125
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.0 0.25 0.25
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.0 0.25 1.0
+
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.0 0.5 0.125
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.0 0.5 0.25
+sbatch scripts/paper_experiments/slurm_lang_pert2.sh 0.0 0.5 1.0
