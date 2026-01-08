14603ea4005114ef9272c84b32ec4878389cf761
Anastasiia Pedan <pedanana@Anastasiias-MacBook-Pro.local>
2026-01-08 09:55:48 -0700

    added scripts for reppo + pert

---
+++ scripts/paper_experiments/run_pert_ent_var_reppo.sh
@@ -0,0 +1,29 @@
+# no pert (to double check)
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.0 0.125
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.0 0.25
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.0 0.5
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.0 1.0
+
+
+# body perturbations
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.125 0.0 0.125
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.125 0.0 0.25
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.125 0.0 0.5
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.125 0.0 1.0
+
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.25 0.0 0.125
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.25 0.0 0.25
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.25 0.0 0.5
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.25 0.0 1.0
+
+
+# gravity perturbations
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.25 0.125
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.25 0.25
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.25 0.5
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.25 1.0
+
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.5 0.125
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.5 0.25
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.5 0.5
+sbatch scripts/paper_experiments/slurm_reppo_pert2.sh 0.0 0.5 1.0
---
+++ scripts/paper_experiments/slurm_reppo_pert2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -N 1            # number of nodes on which to run
+#SBATCH --gres=gpu:1        # number of gpus
+#SBATCH --cpus-per-task=16     # number of cpus required per task
+#SBATCH --mem=128GB
+#SBATCH --ntasks=1
+#SBATCH --tasks-per-node=1
+#SBATCH --time=8:00:00      # time limit
+#SBATCH --account aip-whitem
+#SBATCH --job-name=sac_lang
+#SBATCH --output=slurm_logs/slurm_mjx_op_%A_%a.out
+#SBATCH --error=slurm_logs/slurm_mjx_op_%A_%a.err
+#SBATCH --array=0-26%26
+
+env=(AcrobotSwingup AcrobotSwingupSparse BallInCup CartpoleBalance CartpoleBalanceSparse CartpoleSwingup CartpoleSwingupSparse CheetahRun FingerSpin FingerTurnEasy FingerTurnHard FishSwim HopperHop HopperStand PendulumSwingup ReacherEasy ReacherHard WalkerRun WalkerWalk WalkerStand HumanoidStand HumanoidWalk HumanoidRun HumanoidStand HumanoidWalk HumanoidRun)
+hostname
+
+cd /home/$USER/projects/aip-whitem/pedanana/langevin/reppo
+source .venv/bin/activate
+
+python src/jaxrl/reppo.py --config-name=reppo \
+    env=mjx_dmc_pert \
+    env.name=${env[$((SLURM_ARRAY_TASK_ID%26))]} \
+    seed=$RANDOM \
+    tune=false \
+    env.pert.body_mass_pert=$1 \
+    env.pert.gravity_pert=$2 \
+    hyperparameters.ent_target_mult=$3 \
+    tags=[policy,corr_pert,ent_var]
